f106211a6957774d4d07651d208beddc5d5e3397
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-03-27 10:59:24 -0700

    setting up catch

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -33,10 +33,52 @@ export default function Form (){
 
     })
 
-    const [ errors, setErros ] = useState ({ //4) Set our error state
+    const [ errors, setErrors ] = useState ({ //4) Set our error state
         name:"",
         size:"",
         instructions:"",
         toppings:""
     })
+
+    const [ post, setPost ] = useState([]); //5) Setting state for your post request
+
+    useEffect(()=>{ //6) This is checking the formSchema, which is a model for our form, so every form we create has to follow those rules we've set. It is checked if it is valid vs what the user is putting in. If the user doesn't follow our instructions then it won't change the button to light up and be valid.
+        formSchema.isValid(formState).then(valid => {
+            setButton(!valid);
+        })
+    }, [formState]);
+
+    const formSubmit = e => { //6) Form submit state
+        e.preventDefault();
+        axios
+            .post("https://reqres.in/api/orders", formState)
+            .then(response => {
+                setPost(response.data);
+
+                setFormState({
+                    name:"",
+                    size:"",
+                    instructions:"",
+                    toppings:""
+                })
+           })
+           .catch(errors => console.log("something went wrong when submitting your order"));
+    }
+    const validateChange = e => { //7)
+        yup
+            .reach(formSchema, e.target.name)
+            .validate(e.target.valve ===)
+            .then(valid => {
+                setErrors({
+                    ...errors,
+                    [e.target.name]: ""
+                })
+            })
+            .catch(errors =>{
+                setErrors({
+                    ...errors,
+                    [e.target.name]: errors.errors[0]
+                })
+            })
+    }
 };
